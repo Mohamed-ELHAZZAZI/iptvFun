8c2738ccb67823ab13acb8579687bcf053c99cd1
Med <aminemohamedf7@gmail.com>
2023-03-05 01:00:04 +0100

    creating brand slider

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,14 +4,16 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
+  @stack('styles')
 </head>
-<body class="overflow-hidden">
+<body class="overflow-x-hidden">
   
   <x-navigation.nav />
 
   @yield('content')
 
   <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+  @stack('scripts')
   @vite('resources/js/app.js')
 </body>
 </html>
