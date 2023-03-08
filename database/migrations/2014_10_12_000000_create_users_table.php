88306a59e654eb65867b6b2b8b9d4e7d24ca55aa
Med <aminemohamedf7@gmail.com>
2023-03-09 00:54:11 +0100

    generating uuid in user observer

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -16,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->char('user_uuid', 36)->default(DB::raw('(UUID())')); // Use DB::raw to generate UUID
+            $table->uuid('user_uuid')->nullable()->unique();
             $table->string('firstName');
             $table->string('lastName');
             $table->string('email')->unique();
